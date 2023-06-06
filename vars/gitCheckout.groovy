912b2f2d849290b35a28529b3de0eccedf55fb47
Nasir <nasirpatel1957@gmail.com>
2023-06-06 19:21:55 +0530

    modified 2

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,7 +1,9 @@
 def call(Map stageParams) {
-    checkout {
-        $class: 'GitSCM'
-        branches: [[name: stageParams.branch]]
-        userRemoteConfigs: [[url: stageParms.url]] #ref: https://devopscube.com/create-jenkins-shared-library/
-    }
+    checkout {[
+        $class: 'GitSCM',
+        branches: [[name: stageParams.branch]],
+        userRemoteConfigs: [[url: stageParms.url]] 
+    ]}
 }
+
+#ref: https://devopscube.com/create-jenkins-shared-library/
