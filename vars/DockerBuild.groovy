ca7e339cccae546c07be5a17234226a032f7686e
Nasir <nasirpatel1957@gmail.com>
2023-06-07 20:10:12 +0530

    added DockerBuild groovy file

---
+++ vars/DockerBuild.groovy
@@ -0,0 +1,6 @@
+def call(String hubUser, String project, String version){
+    sh """
+        docker build -t ${hubUser}/${project} .
+        docker image tag ${hubUser}/${project} ${hubUser}/${project}:${version}
+    """
+}
